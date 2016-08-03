4a8d1af3b1cdefe6fd6d534f8263fe0d910f5330
Joseph Sheldon Jr <kb3vwt@gmail.com>
2016-08-03 16:06:22 -0400

    Added empty ex_fit.py file

---
+++ ex_fit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#This Example shows how to use emcee with the SLCTM class.
+
+import math
+import sys
+import numpy as np
+import batman
+import matplotlib.pyplot as plt
+import emcee
+
+import SLCTM
+
+#To Start!
